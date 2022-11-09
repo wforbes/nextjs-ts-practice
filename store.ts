47b192fbab7e1be50f0075e1643a7129889a2cbd
Will Forbes <wforbes87@gmail.com>
2022-11-08 23:32:31 -0800

    add redux hooks, example slice, and update store file

--- store.ts
+++ store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './slices/counterSlice'
 
 export const store = configureStore({
-	reducer: {}
+	reducer: {
+		counter: counterReducer
+	}
 })
 
 export type RootState = ReturnType<typeof store.getState>
