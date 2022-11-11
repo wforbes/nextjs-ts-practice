8d867d6bff429d4bf245031005c3d97c3497031c
Will Forbes <wforbes87@gmail.com>
2022-11-11 15:37:34 -0800

    github pages: last try...

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   images: {
     //https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
     unoptimized: true
   },
-  assetPrefix: isProd ? "/nextjs-ts-practice/" : "",
-  basePath: isProd ? "/nextjs-ts-practice" : "",
+  assetPrefix: "/nextjs-ts-practice/",
+  basePath: "/nextjs-ts-practice",
 }
 
 export default nextConfig
