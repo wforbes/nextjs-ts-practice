ce47b09c207b4d0fe453cebc561537102daa68e2
Will Forbes <wforbes87@gmail.com>
2022-11-12 11:00:48 -0800

    move route checker hook into hooks file

--- components/MainLayout/index.tsx
+++ components/MainLayout/index.tsx
@@ -1,26 +1,11 @@
+import { ReactNode } from 'react'
 import Head from 'next/head'
 import Header from './Header'
 import { Main } from './styles'
-//import styles from '../../styles/Home.module.css'
-//import Footer from './footer'
-import { ReactNode, useEffect } from 'react'
-import { useRouter } from 'next/router'
+//import { useRouteChecker } from '../../hooks'
 
 export default function Layout({ children }: { children: ReactNode }) {
-	// temporary: sorting out how routes are changing on gh-pages
-	const router = useRouter()
-	useEffect(() => {
-		const handleRouteChange = (url: any, { shallow }: any) => {
-			console.log(`Changing to ${url} ${shallow ? 'with' : 'without'} shallow routing`)
-		}
-
-		router.events.on('routeChangeStart', handleRouteChange)
-
-		return () => {
-			router.events.off('routeChangeStart', handleRouteChange)
-		}
-	}, [router])
-
+	//useRouteChecker();
 	return (
 		<>
 			<Head>
--- hooks.ts
+++ hooks.ts
@@ -1,6 +1,25 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 // Use throughout app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+// temporary: sorting out how routes are changing on gh-pages
+export const useRouteChecker: () => void = () => {
+	const router = useRouter()
+	useEffect(() => {
+		const handleRouteChange = (url: any, { shallow }: any) => {
+			console.log(`Changing to ${url} ${shallow ? 'with' : 'without'} shallow routing`)
+		}
+
+		router.events.on('routeChangeStart', handleRouteChange)
+
+		return () => {
+			router.events.off('routeChangeStart', handleRouteChange)
+		}
+	}, [router])
+}
+
