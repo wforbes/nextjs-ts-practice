50cd2ead6fd088f24b4133125db1f1872c81d4b5
Will Forbes <wforbes87@gmail.com>
2022-11-12 11:00:12 -0800

    move ToolbarLogo styles into external file

--- components/MainLayout/ToolbarLogo.tsx
+++ components/MainLayout/ToolbarLogo.tsx
@@ -1,54 +1,21 @@
 import React from 'react'
-import { createTheme, responsiveFontSizes } from '@mui/material/styles'
-import Typography from '@mui/material/Typography'
-import Link from 'next/link'
-
+import Image from 'next/image.js'
 import MuiSvg from '../../assets/mui-svg.jsx'
 import { createSvgIcon } from '@mui/material/utils'
 import SvgIcon from '@mui/material/SvgIcon'
-import reactLogo from '/react.svg'
-import styled from '@emotion/styled'
-import Image from 'next/image.js'
+import {
+	AppBarLogoText,
+	AppBarLogoLink,
+	AppBarLogoLParen,
+	AppBarLogoDash,
+	IconLink,
+	AppBarLogoRParen
+} from './styles'
 
 const MuiIcon = createSvgIcon(
 	<MuiSvg />,
 	'MuiIcon'
 )
-
-
-const AppBarLogoText = styled(Typography)`
-	display: inline-block;
-`
-
-const AppBarLogoLink = styled(Link)`
-	float: left;
-	display: inline-block;
-	color: white;
-	text-decoration: none;
-	margin-right: 0.15em;
-`
-
-const AppBarLogoLParen = styled(Typography)`
-	margin: 0 0.1em 0.2em 0.1em !important;
-	display: inline-block;
-`
-
-const AppBarLogoDash = styled(Typography)`
-	margin: 0 0.15em 0.2em 0.1em !important;
-	display: inline-block;
-`
-const IconLink = styled.a`
-	position: relative;
-	top: 0.4em!important;
-	width: 2rem;
-	height: 3rem;
-	display: inline-block;
-`
-
-const AppBarLogoRParen = styled(Typography)`
-	margin: 0 0.1em 0.2em 0.15em !important;
-	display: inline-block;
-`
 
 export default function ToolbarLogo() {
 	return (
--- components/MainLayout/styles.ts
+++ components/MainLayout/styles.ts
@@ -1,4 +1,6 @@
-import styled from "@emotion/styled"
+import styled from '@emotion/styled'
+import Typography from '@mui/material/Typography'
+import Link from 'next/link'
 
 export const Main = styled.main`
 	min-height: 100vh;
@@ -8,3 +10,37 @@ export const Main = styled.main`
 	flex-direction: column;
 	align-items: center;
 `;
+
+export const AppBarLogoText = styled(Typography)`
+	display: inline-block;
+`
+
+export const AppBarLogoLink = styled(Link)`
+	float: left;
+	display: inline-block;
+	color: white;
+	text-decoration: none;
+	margin-right: 0.15em;
+`
+
+export const AppBarLogoLParen = styled(Typography)`
+	margin: 0 0.1em 0.2em 0.1em !important;
+	display: inline-block;
+`
+
+export const AppBarLogoDash = styled(Typography)`
+	margin: 0 0.15em 0.2em 0.1em !important;
+	display: inline-block;
+`
+export const IconLink = styled.a`
+	position: relative;
+	top: 0.4em!important;
+	width: 2rem;
+	height: 3rem;
+	display: inline-block;
+`
+
+export const AppBarLogoRParen = styled(Typography)`
+	margin: 0 0.1em 0.2em 0.15em !important;
+	display: inline-block;
+`
