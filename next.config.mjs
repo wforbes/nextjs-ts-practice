51e1ed12a389bd935cd5b9e3a1c58d6693be6477
Will Forbes <wforbes87@gmail.com>
2022-11-12 08:32:21 -0800

    simplify nextConfig for hmr builds
    - add conditional for NODE_ENV var as development value, setting nextConfig to original boilerplate

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,32 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isDev = process.env.NODE_ENV == 'development'
+
+
+let nextConfig = {};
 let assetPrefix = ''
 let basePath = '/'
 
-if (isGithubActions) {
-  // trim off `<owner>/`
+if (isDev) {
+  nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+  }
+} else if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
   assetPrefix = `/${repo}/`
   basePath = `/${repo}`
-}
-
-const nextConfig = {
-  images: {
-    loader: 'imgix',
-    path: 'https://wforbes.imgix.net'
-  },
-  assetPrefix,
-  basePath
+  
+  nextConfig = {
+    images: {
+      loader: 'imgix',
+      path: 'https://wforbes.imgix.net'
+    },
+    assetPrefix,
+    basePath
+  }
 }
 
 export default nextConfig
